e7b20c36eeae972ba7b232d735f0c9f5ad5ee878
Yuliya <iksulia1@gmail.com>
2023-05-28 18:14:06 +0300

    Initial commit

---
+++ app/src/main/java/com/obushko/numismatistapp/MainActivity.java
@@ -0,0 +1,14 @@
+package com.obushko.numismatistapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
