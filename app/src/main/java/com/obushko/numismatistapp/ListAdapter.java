f4c8b0e0cb1a2c4f7cc721801305018495f80aa0
Yuliya <iksulia1@gmail.com>
2023-05-28 18:53:13 +0300

    add ListAdapter

---
+++ app/src/main/java/com/obushko/numismatistapp/ListAdapter.java
@@ -0,0 +1,61 @@
+package com.obushko.numismatistapp;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class ListAdapter extends RecyclerView.Adapter<ListAdapter.ListViewHolder> {
+
+    private final Context context;
+    private final ArrayList<ListItem> list;
+
+    public ListAdapter(Context context, ArrayList<ListItem> list) {
+        this.context = context;
+        this.list = list;
+    }
+
+    @NonNull
+    @Override
+    public ListViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.card_item, parent, false);
+        return new ListViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ListViewHolder holder, int position) {
+        ListItem currentItem = list.get(position);
+
+        String urlImage = currentItem.getUrlImage();
+        String title = currentItem.getTitle();
+        String price = currentItem.getPrice();
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+
+    public static class ListViewHolder extends RecyclerView.ViewHolder{
+
+        ImageView imageView;
+        TextView textViewTitle;
+        TextView textViewPrice;
+
+        public ListViewHolder(@NonNull View itemView) {
+            super(itemView);
+            imageView = itemView.findViewById(R.id.imageView);
+            textViewTitle = itemView.findViewById(R.id.textViewTitle);
+            textViewPrice = itemView.findViewById(R.id.textViewPrice);
+
+        }
+    }
+}
---
+++ app/src/main/java/com/obushko/numismatistapp/ListItem.java
@@ -0,0 +1,32 @@
+package com.obushko.numismatistapp;
+
+public class ListItem {
+
+    String urlImage;
+    String title;
+    String price;
+
+    public String getUrlImage() {
+        return urlImage;
+    }
+
+    public void setUrlImage(String urlImage) {
+        this.urlImage = urlImage;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+}
